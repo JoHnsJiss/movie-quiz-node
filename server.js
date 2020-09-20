5ba551e4f78776a555119e3dc20de78add69da3e
JIssmon <jissmon11@gmail.com>
2020-09-21 01:10:51 +0530

    nextprob

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ app.use(
     extended: true,
   })
 );
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 //DB config
 const connecton_url =
